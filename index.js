862cb8c0cffadc0a629429b565051c055e4a717a
mushfichowdhury <chowdhurymushfi@gmail.com>
2020-10-07 10:32:23 -0400

    Finished keydown event listener

--- index.js
+++ index.js
@@ -2,5 +2,25 @@ document.addEventListener("DOMContentLoaded", function(){
   createGrid()
   renderBot(currentPosition)
 
-  // ADD CODE HERE!
+  const movesContainer = document.getElementById('moves-container')
+  document.addEventListener("keydown", function(e) {
+    console.log(e.code)
+    const newDirection = document.createElement("li")
+    if (e.code === "ArrowUp") {
+      newDirection.innerText = "up" 
+    }
+    else if (e.code === "ArrowRight") {
+      newDirection.innerText = "right" 
+    }
+    else if (e.code === "ArrowDown") {
+      newDirection.innerText = "down" 
+    }
+    else if (e.code === "ArrowLeft") {
+      newDirection.innerText = "left" 
+    }
+    else {
+      return
+    }
+      movesContainer.append(newDirection)
+  })
 })
